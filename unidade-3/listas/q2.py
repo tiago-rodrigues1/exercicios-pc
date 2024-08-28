196ef197fc666f815d1b5de0d6818dc90ebfad8c
tiago-rodrigues1 <tiagords127@gmail.com>
2024-08-27 23:03:04 -0300

    add q2 de listas

--- unidade-3/listas/q2.py
+++ unidade-3/listas/q2.py
@@ -2,18 +2,33 @@ n = int(input())
 
 valores = []
 indices = []
+montantes = []
 
 total = 0
 
 for i in range(n):
     [valor, indice] = input().split(" ")
 
+    valores.append(float(valor))
+    indices.append(indice)
+
     total += float(valor)
+
+ganhador = input()
+
+apostas_vencedoras = 0
+for i in range(n):
+    montantes.append(0)
+
+    if indices[i] == ganhador:
+        apostas_vencedoras += valores[i]
+
+
+
 
 petisco = int(total * 0.1)
 total -= petisco
 
 
 
-ganhador = input()
 
